7099c419ca5ee577065e7b0365dcf891d9ac3b42
Stanislav K <katasonovsd@yandex.ru>
2022-04-18 07:40:52 +0300

    HW 2 is done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,21 +3,59 @@
     <h1>{{ msg }}</h1>
     {{ error }}
     <br>
-    <input v-model.number="operand1"/>
+    <input v-model.number="operand1" :class="picked"/>
     и
-    <input v-model.number="operand2"/>
+    <input v-model.number="operand2" :class="picked"/>
     =
     {{ result }}
     <hr>
-    <button @click="sum">+</button>
-    <button @click="minus">-</button>
-    <button @click="sub">/</button>
+    <button @click="add">+</button>
+    <button @click="substract">-</button>
+    <button @click="divide">/</button>
     <button @click="multiply">x</button>
     <button @click="pow">Степень</button>
-<!--возведение в степень-->
-<!--    не делим на ноль-->
 
+    <br>
 
+    <input type="checkbox" id="checkbox" v-model="numPad">
+    <label for="checkbox">Показать панель</label>
+
+    <div v-if="picked == 'operand1'">
+      <div v-if="numPad">
+        <button
+            @click="edit1(num)"
+            :title="num"
+            v-for="num in nums"
+            :key="num"
+        > {{ num }}
+        </button>
+        <button @click="del1">Del</button>
+        <div>
+          <input type="radio" id="one" value="operand1" v-model="picked" >
+          <label for="one">Один</label>
+          <input type="radio" id="two" value="operand2" v-model="picked" >
+          <label for="two">Два</label>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <div v-if="numPad">
+        <button
+            @click="edit2(num)"
+            :title="num"
+            v-for="num in nums"
+            :key="num"
+        > {{ num }}
+        </button>
+        <button @click="del2">Del</button>
+        <div>
+          <input type="radio" id="one" value="operand1" v-model="picked" >
+          <label for="one">Один</label>
+          <input type="radio" id="two" value="operand2" v-model="picked" >
+          <label for="two">Два</label>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,27 +71,51 @@ export default {
       operand1: '',
       operand2: '',
       error: '',
+      numPad: true,
+      nums: [1,2,3,4,5,6,7,8,9,0],
+      picked:'operand1',
+      test:'',
     }
   },
   methods: {
-    sum () {
-      this.result = this.operand1 + this.operand2
+    add () {
+      this.result = +this.operand1 + +this.operand2
     },
-    minus () {
+    substract () {
       this.result = this.operand1 - this.operand2
     },
-    sub () {
-      if (this.operand2 !== 0)
-      this.result = this.operand1 / this.operand2
-      else (this.error = 'Нельзя делить на ноль!')
+    divide () {
+      if (this.operand2 !== 0) {
+        this.result = this.operand1 / this.operand2
+      } else {
+        this.error = 'Нельзя делить на ноль!'
+      }
       setTimeout(() => {this.error = ''}, 2000) // Убрали инфу об ошибке
     },
     multiply () {
-      this.result = this.operand1 / this.operand2
+      this.result = this.operand1 * this.operand2
     },
     pow () {
       this.result = this.operand1 ** this.operand2
-    }
+    },
+    edit1(num) {
+      this.test = String(this.operand1)
+      this.operand1 = this.test.padEnd(this.operand1.length + 1,num)
+      console.log(typeof this.operand1)
+    },
+    del1() {
+      this.test = String(this.operand1)
+      this.operand1 = this.test.slice(0,-1)
+      console.log(typeof this.operand1)
+    },
+    edit2(num) {
+      this.test = String(this.operand2)
+      this.operand2 = this.test.padEnd(this.operand2.length + 1,num)
+    },
+    del2() {
+      this.test = String(this.operand2)
+      this.operand2 = this.test.slice(0,-1)
+    },
   }
 }
 
